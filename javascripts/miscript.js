d65583e191300be48dc640d6334f8d06da2f6320
carmenluyo <c.luyorosales@gmail.com>
2016-01-25 16:02:00 -0500

    add page of space and exibhitions

--- javascripts/miscript.js
+++ javascripts/miscript.js
@@ -1,20 +1,3 @@
-// deslis lento
-
-
-$(function() {
-      $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top
-            }, 1000);
-            return false;
-          }
-        }
-      });
-});
 
 
 $.fn.extend({
